5d41f15a8fd1ff4a66a6d02b48668fb1cbb37cd4
devinjordan <134808091+devinjordan@users.noreply.github.com>
2023-11-17 22:00:56 -0500

    First prototype

---
+++ script.js
@@ -0,0 +1,51 @@
+function getComputerChoice() {
+  random = Math.random();
+  let computerSelection;
+  if (random <= 0.333) {
+    computerSelection = 'rock';
+  } else if (random > 0.333 && random < 0.667) {
+    computerSelection = 'paper';
+  } else {
+    computerSelection = 'rock';
+  }
+  return computerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log('Tie game!');
+    return('Tie game!');
+  } else if (playerSelection == 'rock' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'scissors' || playerSelection == 'scissors' && computerSelection == 'rock') {
+    console.log(`You lose the round! ${computerSelection} beats ${playerSelection}`);
+    return false;
+  } else {
+    console.log(`You win the round! ${playerSelection} beats ${computerSelection}`);
+    return true;
+  }
+}
+
+function game() {
+  let playerScore;
+  let computerScore;
+
+  for (i = 0; i <5; i++) {
+    let playerSelection = prompt("Make a choice: ");
+    playerSelection = playerSelection.toLowerCase();
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+    if (result === 'Tie game!') {
+      i--;
+      continue;
+    } else if (result) {
+      playerScore++;
+    } else {
+      computerScore++;
+    }
+
+    if (playerScore === 3) {
+      console.log("You've won the game!");
+    } else if (computerScore === 3) {
+      console.log('Game over! The computer has won.')
+    }
+  }
+}
